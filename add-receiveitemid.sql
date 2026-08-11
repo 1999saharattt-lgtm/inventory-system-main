4ecb5aee0ba877f45581ff2ccfc34021917a26c7
Saharat Chansubin <saharat.c@anamai.moph.go.th>
2026-08-11 11:06:26 +0700

    fix: add receiveItemId to IssueItem

---
+++ add-receiveitemid.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "IssueItem" ADD COLUMN IF NOT EXISTS "receiveItemId" INTEGER;
+ALTER TABLE "IssueItem" ADD CONSTRAINT "IssueItem_receiveItemId_fkey" FOREIGN KEY ("receiveItemId") REFERENCES "ReceiveItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
