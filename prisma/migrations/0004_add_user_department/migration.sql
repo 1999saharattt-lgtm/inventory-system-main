6504d0b4969a909cded78bdd2ff61eb3274de4be
Saharat Chansubin <saharat.c@anamai.moph.go.th>
2026-08-19 13:28:56 +0700

    fix user department migration

---
+++ prisma/migrations/0004_add_user_department/migration.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "User"
+ADD COLUMN "departmentId" INTEGER;
+
+ALTER TABLE "User"
+ADD CONSTRAINT "User_departmentId_fkey"
+FOREIGN KEY ("departmentId")
+REFERENCES "Department"("id")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
